51df892ea93da9b90a41f7d3870c4c6585dac27c
Cao007 <1114685490@qq.com>
2024-05-18 18:20:38 +0800

    动态组件（tab栏）

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,72 @@
 <template>
-  <div>
-    <Tree :treeData="treeData" />
+  <div class="tab">
+    <div class="tab-titles">
+      <div class="tab-title-item" v-for="(item, index) in data" :key="item.name"
+        :class="[curIndex === index ? 'active' : '']" @click="handleClick(item, index)">
+        {{ item.name }}
+      </div>
+    </div>
+    <component :is="curComp"></component>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-interface Tree {
-  name: string,
-  checked?: boolean,
-  children?: Tree[]
-}
+import { ref, shallowRef, markRaw } from 'vue';
+import TestA from './components/TestA.vue';
+import TestB from './components/TestB.vue';
+import TestC from './components/TestC.vue';
 
-const treeData = ref<Tree[]>([
+// 渲染的组件和数据
+const data = ref([
   {
-    name: '1',
-    checked: false,
-    children: [
-      {
-        name: '1-1',
-        checked: false
-      },
-    ]
+    name: 'TestA',
+    comp: markRaw(TestA)
   },
   {
-    name: '2',
-    checked: false,
-    children: [
-      {
-        name: '2-1',
-        checked: false
-      },
-      {
-        name: '2-2',
-        checked: false
-      }
-    ]
+    name: 'TestB',
+    comp: markRaw(TestB)
   },
   {
-    name: '3',
-    checked: false,
-    children: [
-      {
-        name: '3-1',
-        checked: false,
-        children: [
-          {
-            name: '3-1-1',
-            checked: false
-          },
-          {
-            name: '3-1-2',
-            checked: false
-          }
-        ]
-      }
-    ]
+    name: 'TestC',
+    comp: markRaw(TestC)
   }
-]);
+])
 
+// 当前组件和下标
+let curComp = shallowRef(TestA)
+let curIndex = ref(0)
+
+// 点击切换组件
+const handleClick = (item: any, index: number) => {
+  curComp.value = item.comp
+  curIndex.value = index
+}
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.tab {
+  width: 400px;
+  height: 200px;
+
+  .tab-titles {
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid #ccc;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+
+    .tab-title-item {
+      border: 1px solid #ccc;
+      width: 100px;
+      height: 100%;
+      text-align: center;
+      cursor: pointer;
+    }
+  }
+}
+
+.active {
+  background-color: pink;
+}
+</style>
---
+++ src/components/TestC.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tab-content">TestC</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+</script>
+
+<style scoped>
+.tab-content {
+    width: 100%;
+    height: 160px;
+    border: 1px solid #ccc;
+}
+</style>
